403e68aa912d182cc761f68ea5df7c5d9a4fb0ed
Bander Alshammari <bannadr1@hotmail.com>
2018-09-15 18:56:39 +0300

    Step1 routes in seperate file

--- index.js
+++ index.js
@@ -3,14 +3,9 @@ const app = express();
 const Blockchain = require('./blockchain');
 const Block = require('./block');
 const blockchain = new Blockchain();
-const bitcoin = require('bitcoinjs-lib');
-const bitcoinMessage = require('bitcoinjs-message');
-const httpStatus = require('http-status-codes');
-const httpStatusMsg = require('http-status-code');
+const step1Routes = require('./routes/step1')
 
 
-
-const validationRoutine = []
 app.use(express.json())
 
 app.get('/block/:blockHeight', (req, res, next) => {
@@ -33,84 +28,7 @@ app.post('/block', (req, res, next) => {
     }).catch(next)
 })
 
-app.post('/requestValidation', (req, res, next) => {
-    if (req.body.address == undefined) {
-        return res.status(httpStatus.BAD_REQUEST)
-            .json({
-                error: httpStatusMsg.getMessage(httpStatus.BAD_REQUEST)
-            })
-    }
-    let requestTimeStamp = new Date().getTime().toString().slice(0, -3);
-    let address = req.body.address
-    let elem = validationRoutine.find((element) => {
-        return element.address == address
-    })
-    if (elem != undefined && elem.requestTimeStamp > 0) {
-        let requestTimeStamp = elem.requestTimeStamp
-        return res.json({
-            address: address,
-            requestTimeStamp: requestTimeStamp,
-            message: `${address}:${requestTimeStamp}:starRegistry`,
-            validationWindow: requestTimeStamp - new Date().getTime().toString().slice(0, -3) + 300 <= 0 ? 0 : requestTimeStamp - new Date().getTime().toString().slice(0, -3) + 300
-        })
-    }
-    if (elem != undefined) {
-        validationRoutine.pop({ address: address })
-    }
-    validationRoutine.push({ address: address, requestTimeStamp: requestTimeStamp })
-    res.json({
-        address: address,
-        requestTimeStamp: requestTimeStamp,
-        message: `${address}:${requestTimeStamp}:starRegistry`,
-        validationWindow: requestTimeStamp - new Date().getTime().toString().slice(0, -3) + 300 <= 0 ? 0 : requestTimeStamp - new Date().getTime().toString().slice(0, -3) + 300
-    })
-})
-
-app.post('/message-signature/validate', (req, res, next) => {
-    //address, signature 
-    if (req.body.address == undefined || req.body.signature == undefined) {
-        return res.status(httpStatus.BAD_REQUEST)
-            .json({
-                error: httpStatusMsg.getMessage(httpStatus.BAD_REQUEST)
-            })
-    }
-    let address = req.body.address
-    let elemInValidationRoutine = validationRoutine.find((element) => {
-        return element.address == address
-    })
-    if (elemInValidationRoutine == undefined) {
-        return res.status(404).json({ error: 'No entery' })
-    }
-    let requestTimeStamp = elemInValidationRoutine.requestTimeStamp
-    let signingTimeStamp = new Date().getTime().toString().slice(0, -3);
-
-    if (requestTimeStamp - signingTimeStamp + 300 <= 0) {
-        return res.status(401).json({ error: 'Time out' })
-    }
-    //verify
-    let message = `${address}:${requestTimeStamp}:starRegistry`
-    let signature = req.body.signature
-    console.log(bitcoinMessage.verify(message, address, signature))
-    let registerStar = false;
-    let messageSignature = 'invalid';
-    //if verified
-    if (bitcoinMessage.verify(message, address, signature)) {
-        registerStar = true
-        messageSignature = 'valid'
-        validationRoutine.pop({ address: address })
-    }
-    res.json({
-        registerStar: registerStar,
-        status: {
-            address: address,
-            requestTimeStamp: requestTimeStamp,
-            message: message,
-            validationWindow: requestTimeStamp - signingTimeStamp + 300 <= 0 ? 0 : requestTimeStamp - signingTimeStamp + 300,
-            messageSignature: messageSignature
-        }
-    })
-
-})
+app.use(step1Routes)
 
 app.use((err, req, res, next) => {
     res.json({ error: err.message })
---
+++ routes/step1.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const router = express.Router()
+const validationRoutine = []
+const bitcoin = require('bitcoinjs-lib');
+const bitcoinMessage = require('bitcoinjs-message');
+const httpStatus = require('http-status-codes');
+const httpStatusMsg = require('http-status-code');
+
+router.post('/requestValidation', (req, res, next) => {
+    if (req.body.address == undefined) {
+        return res.status(httpStatus.BAD_REQUEST)
+            .json({
+                error: httpStatusMsg.getMessage(httpStatus.BAD_REQUEST)
+            })
+    }
+    let requestTimeStamp = new Date().getTime().toString().slice(0, -3);
+    let address = req.body.address
+    let elem = validationRoutine.find((element) => {
+        return element.address == address
+    })
+    if (elem != undefined && elem.requestTimeStamp > 0) {
+        let requestTimeStamp = elem.requestTimeStamp
+        return res.json({
+            address: address,
+            requestTimeStamp: requestTimeStamp,
+            message: `${address}:${requestTimeStamp}:starRegistry`,
+            validationWindow: requestTimeStamp - new Date().getTime().toString().slice(0, -3) + 300 <= 0 ? 0 : requestTimeStamp - new Date().getTime().toString().slice(0, -3) + 300
+        })
+    }
+    if (elem != undefined) {
+        validationRoutine.pop({ address: address })
+    }
+    validationRoutine.push({ address: address, requestTimeStamp: requestTimeStamp })
+    res.json({
+        address: address,
+        requestTimeStamp: requestTimeStamp,
+        message: `${address}:${requestTimeStamp}:starRegistry`,
+        validationWindow: requestTimeStamp - new Date().getTime().toString().slice(0, -3) + 300 <= 0 ? 0 : requestTimeStamp - new Date().getTime().toString().slice(0, -3) + 300
+    })
+})
+
+router.post('/message-signature/validate', (req, res, next) => {
+    //address, signature 
+    if (req.body.address == undefined || req.body.signature == undefined) {
+        return res.status(httpStatus.BAD_REQUEST)
+            .json({
+                error: httpStatusMsg.getMessage(httpStatus.BAD_REQUEST)
+            })
+    }
+    let address = req.body.address
+    let elemInValidationRoutine = validationRoutine.find((element) => {
+        return element.address == address
+    })
+    if (elemInValidationRoutine == undefined) {
+        return res.status(404).json({ error: 'No entery' })
+    }
+    let requestTimeStamp = elemInValidationRoutine.requestTimeStamp
+    let signingTimeStamp = new Date().getTime().toString().slice(0, -3);
+
+    if (requestTimeStamp - signingTimeStamp + 300 <= 0) {
+        return res.status(401).json({ error: 'Time out' })
+    }
+    //verify
+    let message = `${address}:${requestTimeStamp}:starRegistry`
+    let signature = req.body.signature
+    console.log(bitcoinMessage.verify(message, address, signature))
+    let registerStar = false;
+    let messageSignature = 'invalid';
+    //if verified
+    if (bitcoinMessage.verify(message, address, signature)) {
+        registerStar = true
+        messageSignature = 'valid'
+        validationRoutine.pop({ address: address })
+    }
+    res.json({
+        registerStar: registerStar,
+        status: {
+            address: address,
+            requestTimeStamp: requestTimeStamp,
+            message: message,
+            validationWindow: requestTimeStamp - signingTimeStamp + 300 <= 0 ? 0 : requestTimeStamp - signingTimeStamp + 300,
+            messageSignature: messageSignature
+        }
+    })
+
+})
+
+module.exports = router
